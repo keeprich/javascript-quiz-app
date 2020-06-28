46b949355b98f20d860a68b0aef3faa8547b3c62
keeprich <keeprich@keeprichs-air.lan>
2020-06-28 13:09:15 +0100

    Almost Done

--- script.js
+++ script.js
@@ -1,28 +1,90 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
-const shuffelQuestions, currentQuestionIndex
+let shuffelQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
 
 startButton.addEventListener('click', startGame);
-
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
    console.log('Game started');
    startButton.classList.add('hide');
-   shuffelQuestions = questions.sort(() => Math.random()- .5)
+   shuffelQuestions = questions.sort(() => Math.random() - .5)
    currentQuestionIndex = 0;
    questionContainerElement.classList.remove('hide');
-
+   setNextQuestion()
 
 }
 
 function setNextQuestion() {
-    alert('Next Question Please')
+    console.log('It works')
+    resetStart()
+    showQestion(shuffelQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
-    alert('Select Answer Please')
+function showQestion(question) {
+    questionElement.innerText = question.question
+    question.answer.forEach(answer => {
+        const button = document
+        .createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+
 }
 
+function resetStart() {
+    clearStatusClass(document.body)
+nextButton.classList.add('hide')
+while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+}
+}
+ 
+
+function selectAnswer(e) {
+    // alert(shuffelQuestions.lenght)
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+            nextButton.classList.remove('hide')
+
+    // if (shuffelQuestions.lenght > currentQuestionIndex + 1) {
+    //     nextButton.classList.remove('hide')
+    // }else {
+    //     startButton.innerText = 'Restart'
+    //     startButton.classList.add('hide')
+    // }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    }else{
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+
+}
 
 const questions = [
     {
@@ -33,6 +95,45 @@ const questions = [
             },
             {
                 text: '22', correct:false
+
+            }
+        ]
+    },
+
+    {
+        question: 'What is 7 + 2',
+        answer: [
+            {
+                text: '9', correct:true
+            },
+            {
+                text: '24', correct:false
+
+            }
+        ]
+    },
+
+    {
+        question: 'What is 2 + 10',
+        answer: [
+            {
+                text: '12', correct:true
+            },
+            {
+                text: '62', correct:false
+
+            }
+        ]
+    },
+
+    {
+        question: 'What is 4 + 2',
+        answer: [
+            {
+                text: '6', correct:true
+            },
+            {
+                text: '2', correct:false
 
             }
         ]
